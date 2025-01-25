7469f3d2b971676ecd028c736a72322ebad1aaa3
kgugunava <k.gugunava01@gmail.com>
2025-01-25 23:55:45 +0300

    plcemark работает в проекте

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
 
+func placemarkHandle(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/placemark.html")
+	tmpl.ExecuteTemplate(w, "placemark", nil)
+}
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/login", loginHandle)
+	http.HandleFunc("/placemark", placemarkHandle)
 	http.ListenAndServe(":8050", nil)
 }
